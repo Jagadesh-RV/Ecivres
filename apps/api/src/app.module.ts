cacdd1facf496faba94b41e0eb496344037e48f1
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-11 09:37:47 +0530

    Add apps/api/src/app.module.ts

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProvidersModule } from './modules/providers/providers.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { PermissionsModule } from './modules/permissions/permissions.module';
+import { ServicesModule } from './modules/services/services.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { AdminModule } from './modules/admin/admin.module';
+
+@Module({
+  imports: [AuthModule, CustomersModule, BookingsModule, UsersModule, ProvidersModule, PaymentsModule, RolesModule, CategoriesModule, ReviewsModule, PermissionsModule, ServicesModule, NotificationsModule, AdminModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
