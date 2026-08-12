6b2ca48af8ee704a2ec656d8e42e95160e7571ef
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-12 15:50:55 +0530

    feat: implement RolesGuard to restrict route access based on user role metadata

--- apps/api/src/common/guards/roles.guard.ts
+++ apps/api/src/common/guards/roles.guard.ts
@@ -14,7 +14,7 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const request = context.switchToHttp().getRequest();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     const user = request.user;
