3ef4a4fc049c389fdb4dfb3bcdac954f96974c6e
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-18 10:34:18 +0530

    test: add e2e test suite for RBAC and permissions guards validation

--- apps/api/test/rbac.e2e-spec.ts
+++ apps/api/test/rbac.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, Controller, Get, UseGuards } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { RolesGuard } from '../src/common/guards/roles.guard';
 import { Roles } from '../src/common/decorators/roles.decorator';
