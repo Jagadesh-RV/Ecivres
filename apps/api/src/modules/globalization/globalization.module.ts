a9ae9deabf0782d0b75448604954010cbb993bd9
Jagadesh R <jagadeshrvs@gmail.com>
2026-09-15 22:37:49 +0530

    feat(global): expose translation engine REST APIs for marketplace listings

--- apps/api/src/modules/globalization/globalization.module.ts
+++ apps/api/src/modules/globalization/globalization.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MultiCurrencyService } from './multi-currency.service';
 import { RegionalTaxService } from './regional-tax.service';
+import { ContentTranslatorService } from './content-translator.service';
 import { GlobalizationController } from './globalization.controller';
 
 @Module({
   controllers: [GlobalizationController],
-  providers: [MultiCurrencyService, RegionalTaxService],
-  exports: [MultiCurrencyService, RegionalTaxService],
+  providers: [MultiCurrencyService, RegionalTaxService, ContentTranslatorService],
+  exports: [MultiCurrencyService, RegionalTaxService, ContentTranslatorService],
 })
 export class GlobalizationModule {}
