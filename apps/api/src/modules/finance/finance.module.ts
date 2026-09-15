e6b84af0f9a59d6dd2687808e18ec7695922193e
Jagadesh R <jagadeshrvs@gmail.com>
2026-09-15 22:23:46 +0530

    feat(finance): add HTTP endpoints for provider micro-loans and repayment schedules

--- apps/api/src/modules/finance/finance.module.ts
+++ apps/api/src/modules/finance/finance.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EscrowWalletService } from './escrow-wallet.service';
 import { InstantPayoutService } from './instant-payout.service';
 import { TaxReportingService } from './tax-reporting.service';
+import { MicroLoanService } from './micro-loan.service';
 import { FinanceController } from './finance.controller';
 
 @Module({
   controllers: [FinanceController],
-  providers: [EscrowWalletService, InstantPayoutService, TaxReportingService],
-  exports: [EscrowWalletService, InstantPayoutService, TaxReportingService],
+  providers: [EscrowWalletService, InstantPayoutService, TaxReportingService, MicroLoanService],
+  exports: [EscrowWalletService, InstantPayoutService, TaxReportingService, MicroLoanService],
 })
 export class FinanceModule {}
