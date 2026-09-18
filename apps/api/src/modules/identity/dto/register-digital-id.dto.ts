63a9f88231e285bc7c65340d3e0181618211c5b2
Jagadesh R <jagadeshrvs@gmail.com>
2026-09-18 21:40:16 +0530

    feat(identity): add DTO for digital identity wallet registration

---
+++ apps/api/src/modules/identity/dto/register-digital-id.dto.ts
@@ -0,0 +1,23 @@
+﻿import { IsString, IsEnum, IsOptional } from 'class-validator';
+
+export enum IdentityType {
+  INDIVIDUAL_CUSTOMER = 'INDIVIDUAL_CUSTOMER',
+  CERTIFIED_PROVIDER = 'CERTIFIED_PROVIDER',
+  ENTERPRISE_ORG = 'ENTERPRISE_ORG',
+  GOVERNMENT_AUDITOR = 'GOVERNMENT_AUDITOR',
+}
+
+export class RegisterDigitalIdDto {
+  @IsString()
+  userId!: string;
+
+  @IsEnum(IdentityType)
+  identityType!: IdentityType;
+
+  @IsString()
+  didIdentifier!: string;
+
+  @IsOptional()
+  @IsString()
+  publicKeyPem?: string;
+}
